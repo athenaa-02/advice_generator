f5b23bd5c36d7da232df354fed6f87ca0d7d2c4a
natia <natia.karamnishvili369@law.tsu.ge>
2025-04-01 15:42:11 +0400

    connected to API

---
+++ main.js
@@ -0,0 +1,39 @@
+const adviceId = document.querySelector('span')
+const div = document.querySelector('#button')
+
+
+
+function clear(ele){
+ele.innerText = ''
+}
+
+
+
+const randomAdvice = null
+
+const getAdvice = async() =>{
+try{
+ const response = await axios.get(
+    "https://api.adviceslip.com/advice"
+ )
+ const data = response.data.slip
+ let dataId = data.id
+ let oneAdvice = data.advice
+
+ const adviceId = document.querySelector('span')
+ const advice = document.querySelector('p')
+ clear(advice)
+
+ advice.innerText = oneAdvice
+ adviceId.innerText = dataId
+
+
+ 
+
+
+}catch{
+
+}
+}
+
+getAdvice()
